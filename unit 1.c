43760d90776e5ab55057acab4d7c1097610b4e7c
davisezra045-blip <davisezra045@gmail.com>
2026-09-17 20:11:56 +0300

    Add files via upload

---
+++ unit 1.c
@@ -0,0 +1,24 @@
+/*
+Name:DAVIS EZRA
+Reg N.O:CT/100/G/30694/26
+Date :11/9/2026
+Description : UNITS OFFERED THIS SEMESTER
+*/
+#include<stdio.h>
+int main()
+{
+    printf("name: name: DAVIS EZRA\n");
+    printf("Reg N.O:CT/100/G/30694/26\n");
+    printf("date :11/9/2026\n");
+    printf("description: UNITS OFFERED THIS SEMESTER\n");
+
+printf("1. Fundamental of digit technology\n");
+printf("2. Introduction to computer applications\n");
+printf("3. Essentials of accounting\n");
+printf("4. computer systems and organization\n");
+printf("5. analogue elecronics\n");
+printf("6. mathematics for science\n");
+printf("7. introduction to programming and logarithms");
+printf("8. communication and information liracy skills");
+return 0;
+}
